abfba12db65fff196d369b9eb18207d65f1a5762
Njål Gaute Solland <njaal.solland@gmail.com>
2026-08-07 18:48:02 +0200

    feat: export channel evidence adapter

--- src/valo_gateway/protocols/__init__.py
+++ src/valo_gateway/protocols/__init__.py
@@ -1,10 +1,20 @@
 from .adk_go import ADKGoIngress
 from .base import IngressNormalizer, MappingIngress
+from .channels import (
+    ChannelEventEvidence,
+    ChannelEvidenceNormalizer,
+    ChannelInteraction,
+    ChannelKind,
+)
 from .plugins import A2AIngress, ExtAuthzIngress, GRPCIngress, HTTPIngress, MCPIngress
 
 __all__ = [
     "A2AIngress",
     "ADKGoIngress",
+    "ChannelEventEvidence",
+    "ChannelEvidenceNormalizer",
+    "ChannelInteraction",
+    "ChannelKind",
     "ExtAuthzIngress",
     "GRPCIngress",
     "HTTPIngress",
