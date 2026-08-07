f570c406309a8c90797cb95abe218a0d7c28674d
Njål Gaute Solland <njaal.solland@gmail.com>
2026-08-07 11:17:24 +0200

    feat: export ADK Go runtime surface

--- src/valo_gateway/runtime_adapters/__init__.py
+++ src/valo_gateway/runtime_adapters/__init__.py
@@ -1,3 +1,28 @@
 from .adapters import ClaudeRuntime, GoogleRuntime, LocalRuntime, OpenAIRuntime
+from .adk_go import (
+    ADKCredentialBinding,
+    ADKGoInvocationContext,
+    ADKGoRuntime,
+    ADKRegistryRecord,
+    ADKTaskBinding,
+    ADKTaskRunnerGate,
+    ADKToolConfirmation,
+    assert_confirmation_matches,
+    validate_task_fanout,
+)
 
-__all__ = ["ClaudeRuntime", "GoogleRuntime", "LocalRuntime", "OpenAIRuntime"]
+__all__ = [
+    "ADKCredentialBinding",
+    "ADKGoInvocationContext",
+    "ADKGoRuntime",
+    "ADKRegistryRecord",
+    "ADKTaskBinding",
+    "ADKTaskRunnerGate",
+    "ADKToolConfirmation",
+    "ClaudeRuntime",
+    "GoogleRuntime",
+    "LocalRuntime",
+    "OpenAIRuntime",
+    "assert_confirmation_matches",
+    "validate_task_fanout",
+]
